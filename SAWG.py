33151dd31b62bbbe15c2d8d5d3ef030d18349b2c
Noor-143 <164313787+Noor-143@users.noreply.github.com>
2024-09-16 16:11:32 +0600

    Delete SAWG.py

--- SAWG.py
+++
@@ -1,8 +0,0 @@
-import platform,os,time
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import SWAG
-elif bit == '32bit':
-    #import SWAG_old_32
-    while True:
-        print("Now 32 bit is off please wait")
